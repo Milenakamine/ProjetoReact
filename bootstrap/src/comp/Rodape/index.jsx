7053851e82fa92246bb3b4ccb9605ab730dd07c1
Milenakamine <aakaminemilena@gmail.com>
2020-10-21 20:51:45 -0300

    Finalizado

---
+++ bootstrap/src/comp/Rodape/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Rodape(){
+
+    return (
+
+        <footer>
+            <h1>Milena Akamine dos Santos Reis, 
+                Tel: 11 99999-9999
+            </h1>
+        </footer>
+
+    )
+
+}
+
+export default Rodape;
